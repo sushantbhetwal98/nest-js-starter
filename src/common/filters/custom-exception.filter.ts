5645d936188554a778265aaac3fdbc831880c6d9
sushantbhetwal98 <sushanbhetwal@gmail.com>
2024-12-14 18:50:11 +0545

    custom exception created and implemented as global filter

---
+++ src/common/filters/custom-exception.filter.ts
@@ -0,0 +1,53 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+
+@Catch()
+export class CustomExceptionFilter implements ExceptionFilter {
+  constructor(private logger: Logger) {}
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message = this.getErrorMessage(exception);
+    const timestamp = new Date().toISOString();
+
+    this.logger.error(
+      `${request.method} ${request.originalUrl} ${status} ${message}`,
+    );
+
+    console.log(exception);
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp,
+      path: request.url,
+      message,
+    });
+  }
+
+  private getStatusCode(exception: unknown): number {
+    return exception instanceof HttpException
+      ? exception.getStatus()
+      : HttpStatus.INTERNAL_SERVER_ERROR;
+  }
+
+  private getErrorMessage(exception: unknown): string {
+    if (exception instanceof HttpException) {
+      const response = exception.getResponse();
+      return typeof response === 'string' ? response : response['message'];
+    }
+    return 'Internal server error';
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import globalConfiguration from './config/global.config';
+import { CustomExceptionFilter } from './common/filters/custom-exception.filter';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // custom exception filter
+  const logger = new Logger();
+  app.useGlobalFilters(new CustomExceptionFilter(logger));
   await app.listen(globalConfiguration().PORT);
 }
 bootstrap();
