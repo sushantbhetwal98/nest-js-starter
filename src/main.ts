57c56652f1915fdfd066e13b7c1f89e5d56d262e
sushantbhetwal98 <sushanbhetwal@gmail.com>
2024-12-14 19:05:16 +0545

    custom validation pipe added and set as global pipe

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,22 @@ import { AppModule } from './app.module';
 import globalConfiguration from './config/global.config';
 import { CustomExceptionFilter } from './common/filters/custom-exception.filter';
 import { Logger } from '@nestjs/common';
+import { CustomValidationPipe } from './common/pipe/custom-validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // custom exception filter
   const logger = new Logger();
   app.useGlobalFilters(new CustomExceptionFilter(logger));
+  // custom validation pipe
+  app.useGlobalPipes(
+    new CustomValidationPipe({
+      transform: true,
+      whitelist: true,
+      stopAtFirstError: true,
+    }),
+  );
+
   await app.listen(globalConfiguration().PORT);
 }
 bootstrap();
